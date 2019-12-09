89d70618fec9384bf83c3a959527b88f151a6b67
[Hacklogs] <hacklogs@users.noreply.github.com>
2019-12-09 19:42:08 +0500

    Added commands like a '-h'/'-help' for help and '-cmds' for commands in this bot (just wanted it look like command line apps?)

--- index.js
+++ index.js
@@ -9,14 +9,31 @@ client.on("ready", () => {
 client.on("message", msg => {
   if(msg.author.bot)return
 
-  if(msg.content == prefix + " -h"){
-    helpEmbed = new Discord.RichEmbed()
-    .setTitle("help")
-    .setDescription("Sorry, but there is no any information...")
+  if(msg.content == "hl"){
+    hlAll = new Discord.RichEmbed()
+    .setTitle("please type `hl -h` or `-help` to get info")
+    .setColor("#ff0000")
 
-    msg.send(helpEmbed)
+    msg.channel.send(hlAll)
+  }
+
+  if(msg.content == prefix + " -h"){
+    hlHelp = new Discord.RichEmbed()
+    .setTitle ("help")
+    .setColor ("#ff0000")
+    .addField ('-cmds', 'type ' + prefix + ' `-cmds` or `-commands` to get all commands')
+
+    msg.channel.send(hlHelp)
+  }
+  if(msg.content == prefix + " -cmds"){
+    hlCmds = new Discord.RichEmbed()
+    .setTitle("commands")
+    .setDescription("Sorry, there is no any commands yet...")
+    .setColor("#ff0000")
+
+    msg.channel.send(hlCmds)
   }
 })
 
 
-client.login("NjUzMTc3NzUwNTM3ODk1OTUy.Xe5JIQ.05IkebX__YKpObp-VSTIhr1AJyM")
+client.login("NjUzMTc3NzUwNTM3ODk1OTUy.Xe5RzQ.PS4wNEugiEDZyHxJIGGjl2wIC7w")
