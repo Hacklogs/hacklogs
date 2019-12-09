a90f7b28b890840ac5787de7730f2982a840122a
[Hacklogs] <hacklogs@users.noreply.github.com>
2019-12-09 18:45:52 +0500

    Uh.. fixed login

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
 
+prefix = "hl"
+
 client.on("ready", () => {
   console.log(`${client.user.username} is ready!`)
 })
 client.on("message", msg => {
   if(msg.author.bot)return
 
-  // if(msg.content == ""){
-  //
-  // }
+  if(msg.content == prefix + " -h"){
+    helpEmbed = new Discord.RichEmbed()
+    .setTitle("help")
+    .setDescription("Sorry, but there is no any information...")
+
+    msg.send(helpEmbed)
+  }
 })
 
 
-client.login("NjUzMTc3NzUwNTM3ODk1OTUy.Xe4oIQ.i5eLQbocQpEPozWIWr-chUNBMoI")
+client.login("NjUzMTc3NzUwNTM3ODk1OTUy.Xe5JIQ.05IkebX__YKpObp-VSTIhr1AJyM")
